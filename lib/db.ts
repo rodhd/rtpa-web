f6704df7dc0c479be901fa099d907d909bd0c499
Rodrigo Herrera <rodrigo.diaz@workwise.io>
2025-05-23 13:54:21 +0200

    update db connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,10 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import { z } from 'zod';
 import * as schema from '@/lib/schema';
 
 const connectionStringSchema = z.coerce.string()
 const connectionString = connectionStringSchema.parse(process.env.DATABASE_URL)
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString, { prepare: false })
+export const client = neon(connectionString);
 export const db = drizzle(client, {schema});
